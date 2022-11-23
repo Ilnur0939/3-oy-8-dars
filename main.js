afbc9ad549e2951c4da13bce1b6f4bef5b9f4d09
Пользователь <Ilnur0939>
2022-11-23 16:50:12 +0500

    Change of js file

--- main.js
+++ main.js
@@ -3,42 +3,72 @@ let result = document.querySelectorAll('.list__time')
 let distance = document.querySelector('#inp').value;
 // console.log(distance);
 let time = {
-    person: '3.6',
+    person: 3.6,
     bycicle: 20.1,
     car: 70,
-    airplane:800
+    airplane: 800
 }
 
 form.addEventListener('submit', function(evt){
     evt.preventDefault();
     
-    function calc(){
+    function calcPerson(){
         let distance = document.querySelector('#inp').value;
-        let natija = Math.round(distance.split('km')[0] / time['person']);
+        let natija = Math.floor(distance.split('km')[0] / time['person']);
         return natija
     }
+    function minPerson(){
+        distance = document.querySelector('#inp').value;
+        son = distance.split('km')[0] / time['person']
+        natija = Math.trunc(distance.split('km')[0] / time['person']);
+        let qwer = (son.toFixed(1) % natija.toFixed(1)) * 60
+        return qwer.toFixed(0)
+    }  
 
     function calcBy(){
-        let distance = document.querySelector('#inp').value;
-        let natija = Math.round(distance.split('km')[0] / time['bycicle']);
+        distance = document.querySelector('#inp').value;
+        natija = Math.floor(distance.split('km')[0] / time['bycicle']);
         return natija
+    }
+    function minBy(){
+        distance = document.querySelector('#inp').value;
+        son = distance.split('km')[0] / time['bycicle']
+        natija = Math.floor(distance.split('km')[0] / time['bycicle']);
+        qwer = (son.toFixed(1) % natija.toFixed(1)) * 60
+        return qwer.toFixed(0)
     }
 
     function calcCar(){
-        let distance = document.querySelector('#inp').value;
-        let natija = Math.round(distance.split('km')[0] / time['car']);
-        return natija
-    }
-    function calcAirplane(){
-        let distance = document.querySelector('#inp').value;
-        let natija = Math.round(distance.split('km')[0] / time['airplane']);
+        distance = document.querySelector('#inp').value;
+        natija = Math.floor(distance.split('km')[0] / time['car']);
         return natija
     }
 
-    result[0].innerHTML = `${calc()} soat` 
-    result[1].innerHTML = `${calcBy()} soat` 
-    result[2].innerHTML = `${calcCar()} soat` 
-    result[3].innerHTML = `${calcAirplane()} soat` 
+    function minCar(){
+        distance = document.querySelector('#inp').value;
+        son = distance.split('km')[0] / time['car']
+        natija = Math.floor(distance.split('km')[0] / time['car']);
+        qwer = (son.toFixed(1) % natija.toFixed(1)) * 60
+        return qwer.toFixed(0)
+    }
+    function calcAirplane(){
+        distance = document.querySelector('#inp').value;
+        natija = Math.floor(distance.split('km')[0] / time['airplane']);
+        return natija
+    }
+
+    function minAirplane(){
+        distance = document.querySelector('#inp').value;
+        son = distance.split('km')[0] / time['airplane']
+        natija = Math.floor(distance.split('km')[0] / time['airplane']);
+        qwer = (son.toFixed(1) % natija.toFixed(1)) * 60
+        return qwer.toFixed(3)
+    }
+    console.log(minAirplane());
+    result[0].innerHTML = `${calcPerson()} soat ${minPerson()} daqiqa` 
+    result[1].innerHTML = `${calcBy()} soat ${minBy()} daqiqa` 
+    result[2].innerHTML = `${calcCar()} soat ${minCar()} daqiqa` 
+    result[3].innerHTML = `${calcAirplane()} soat ${minAirplane()} daqiqa` 
 
     form.reset();
 })
